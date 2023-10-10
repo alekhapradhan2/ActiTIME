4855562d29a01d19311d5144e1e96e3ef89778bf
Dell <Dell@Alekh>
2023-10-10 15:59:19 +0530

    Some coding error fixed and code execute perfectly

--- ActTIme/src/test/java/TestRunnerBlock/LoginRunner.java
+++ ActTIme/src/test/java/TestRunnerBlock/LoginRunner.java
@@ -3,7 +3,7 @@ package TestRunnerBlock;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "./src/main/java/FeatureFiles",
+@CucumberOptions(features = "./src/main/java/FeatureFiles/Login.feature",
 				 glue="StepDefinationBlock",
 				 dryRun = false)
 public class LoginRunner extends AbstractTestNGCucumberTests  {
